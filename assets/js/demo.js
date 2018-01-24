dc1de97cd2d4fa4e3f558813974eb32acf8e6f89
MrsPepa <jose.leon1996@gmail.com>
2018-01-24 16:38:19 -0300

    DEMO version 1.0

---
+++ assets/js/demo.js
@@ -0,0 +1,23 @@
+`use strict`
+
+function archivo(evt){
+  var files = evt.target.files;
+  //obtener la imagen del campo
+  for(var i = 0, f; f = files[i]; i++) {
+    //solo admitimos imagenes
+    if(!f.type.match('image.*')) {
+      continue;
+    }
+    var reader = new FileReader();
+    reader.onload = (function(theFile){
+      return function(e){
+        //crear imagen
+        document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+      };
+    })(f);
+    reader.readAsDataURL(f);
+  }
+}
+document.getElementById('files').addEventListener('change', archivo, false);
+
+archivo => evt
