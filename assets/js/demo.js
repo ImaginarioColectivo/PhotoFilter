182d58a753bb3abbf7d7e14499e8756cbe42188c
MrsPepa <jose.leon1996@gmail.com>
2018-01-26 17:27:20 -0300

    DEMO version 2.0 final presentation

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -6,7 +6,7 @@ const opacity = document.getElementById('opacity');
 const contrast = document.getElementById('contrast');
 const hueRotate = document.getElementById('hue-rotate');
 const dropShadow = document.getElementById('drop-shadow');
-const none = [];
+const none = document.getElementById('none');
 const invert = document.getElementById('invert');
 const saturate = document.getElementById('saturate');
 const graysacale = document.getElementById('grayscale');
@@ -14,53 +14,54 @@ const brightness = document.getElementById('brightness');
 const photofilter = document.getElementById('photofilter');
 const filter = document.getElementById('filtro');
 
-blur.addEventListener('click', function(){
+blur.addEventListener('click', function(event){
   filter.style.filter = "blur(5px)";
   filter.style.WebkitFilter = "blur(5px)";
+  
 });
-sepia.addEventListener('click', function(){
-  filter.style.filter = "100%";
-  filter.style.WebkitFilter = "100%";
+sepia.addEventListener('click', function(fil){
+  filter.style.filter = "sepia(100%)";
+  filter.style.WebkitFilter = "sepia(100%)";
 });
 opacity.addEventListener('click', function(){
-  filter.style.filter = "30%";
-  filter.style.WebkitFilter = "30%";
+  filter.style.filter = "opacity(50%)";
+  filter.style.WebkitFilter = "opacity(50%)";
 })
 contrast.addEventListener('click', function(){
-  filter.style.filter = "200%";
-  filter.style.WebkitFilter = "200%";
+  filter.style.filter = "contrast(200%)";
+  filter.style.WebkitFilter = "contrast(200%)";
 })
 hueRotate.addEventListener('click', function(){
-  filter.style.filter = "";
-  filter.style.WebkitFilter = "";
+  filter.style.filter = "hue-rotate(90deg)";
+  filter.style.WebkitFilter = "hue-rotate(90deg)";
 })
 dropShadow.addEventListener('click', function(){
-  filter.style.filter = "";
-  filter.style.WebkitFilter = "";
+  filter.style.filter = "drop-shadow(8px 8px 10px gray)";
+  filter.style.WebkitFilter = "drop-shadow(8px 8px 10px teal)";
 })
-/*none.addEventListener('click', function(){
-  filter.style.filter = "";
-  filter.style.WebkitFilter = "";
-})*/
+none.addEventListener('click', function(){
+  filter.style.filter = "none";
+  filter.style.WebkitFilter = "none";
+})
 invert.addEventListener('click', function(){
-  filter.style.filter = "";
-  filter.style.WebkitFilter = "";
+  filter.style.filter = "invert(100%)";
+  filter.style.WebkitFilter = "invert(100%)";
 })
 saturate.addEventListener('click', function(){
-  filter.style.filter = "";
-  filter.style.WebkitFilter = "";
+  filter.style.filter = "saturate(8)";
+  filter.style.WebkitFilter = "saturate(8)";
 })
 grayscale.addEventListener('click', function(){
-  filter.style.filter = "";
-  filter.style.WebkitFilter = "";
+  filter.style.filter = "grayscale(100%)";
+  filter.style.WebkitFilter = "grayscale(100%)";
 })
 brightness.addEventListener('click', function(){
-  filter.style.filter = "";
-  filter.style.WebkitFilter = "";
+  filter.style.filter = "brightness(200%)";
+  filter.style.WebkitFilter = "brightness(200%)";
 })
 photofilter.addEventListener('click', function(){
-  filter.style.filter = "";
-  filter.style.WebkitFilter = "";
+  filter.style.filter = "saturate(3)";
+  filter.style.WebkitFilter = "saturate(3)";
 })
 /*blur.addEventListener('click', function(){
 var thumb = document.getElementById('blah').style.filter = "blur(5px)";
