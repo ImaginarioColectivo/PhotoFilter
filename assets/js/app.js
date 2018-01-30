a6f7cc1708a91ff43f20588adac7925ace12b982
ImaginarioColectivo <marjourzua@gmail.com>
2018-01-30 14:59:47 -0300

    arreglando sintaxis en index.html, demo.html, app.js, demo.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,83 +1,83 @@
 'use strict'
 
-const blur = document.getElementById('blurSelect');
-const brightness = document.getElementById('brightnessSelect');
-const contrast = document.getElementById('contrastSelect');
-const dropShadow = document.getElementById('dropShadowSelect');
-const grayscale = document.getElementById('grayscaleSelect');
-const hueRotate = document.getElementById('hueRotateSelect');
-const invert = document.getElementById('invertSelect');
-const opacity = document.getElementById('opacitySelect');
-const saturate = document.getElementById('saturateSelect');
-const sepia = document.getElementById('sepiaSelect');
-const photofilter = document.getElementById('photofilterSelect');
-const none = document.getElementById('noneSelect');
+const blurSelect = document.getElementById('blur');
+const brightnessSelect = document.getElementById('brightness');
+const contrastSelect = document.getElementById('contrast');
+const dropShadowSelect = document.getElementById('dropShadow');
+const grayscaleSelect = document.getElementById('grayscale');
+const hueRotateSelect = document.getElementById('hueRotate');
+const invertSelect = document.getElementById('invert');
+const opacitySelect = document.getElementById('opacity');
+const saturateSelect = document.getElementById('saturate');
+const sepiaSelect = document.getElementById('sepia');
+const photofilterSelect = document.getElementById('photofilter');
+const noneSelect = document.getElementById('none');
 
 blurSelect.addEventListener('click', function(){
-document.getElementById('blur').style.filter = 'blur(2px)';
+document.getElementById('blurImg').style.filter = 'blur(2px)';
 // Code for Safari 6.0 - 9.0
-document.getElementById('blur').style.WebkitFilter = 'blur(2px)';
+document.getElementById('blurImg').style.WebkitFilter = 'blur(2px)';
 });
 
 brightnessSelect.addEventListener('click', function(){
-document.getElementById('brightness').style.filter = 'brightness(190%)';
+document.getElementById('brightnessImg').style.filter = 'brightness(190%)';
 // Code for Safari 6.0 - 9.0
-document.getElementById('brightness').style.WebkitFilter = 'brightness(190%)';
+document.getElementById('brightnessImg').style.WebkitFilter = 'brightness(190%)';
 });
 
 contrastSelect.addEventListener('click', function(){
-document.getElementById('contrast').style.filter = 'contrast(300%)';
+document.getElementById('contrastImg').style.filter = 'contrast(300%)';
 // Code for Safari 6.0 - 9.0
-document.getElementById('contrast').style.WebkitFilter = 'contrast(300%)';
+document.getElementById('contrastImg').style.WebkitFilter = 'contrast(300%)';
 });
 
 dropShadowSelect.addEventListener('click', function(){
-document.getElementById('drop-shadow').style.filter = 'drop-shadow(10px 8px 20px red)';
+document.getElementById('drop-shadowImg').style.filter = 'drop-shadow(10px 8px 20px red)';
 // Code for Safari 6.0 - 9.0
-document.getElementById('drop-shadow').style.WebkitFilter = 'drop-shadow(10px 8px 20px red)';
+document.getElementById('drop-shadowImg').style.WebkitFilter = 'drop-shadow(10px 8px 20px red)';
 });
 
 grayscaleSelect.addEventListener('click', function(){
-document.getElementById('grayscale').style.filter = 'grayscale(100%)';
+document.getElementById('grayscaleImg').style.filter = 'grayscale(100%)';
 // Code for Safari 6.0 - 9.0
-document.getElementById('grayscale').style.WebkitFilter = 'grayscale(100%)';
+document.getElementById('grayscaleImg').style.WebkitFilter = 'grayscale(100%)';
 });
 
 hueRotateSelect.addEventListener('click', function(){
-document.getElementById('hue-rotate').style.filter = 'hue-rotate(90deg)';
+document.getElementById('hue-rotateImg').style.filter = 'hue-rotate(90deg)';
 // Code for Safari 6.0 - 9.0
-document.getElementById('hue-rotate').style.WebkitFilter = 'hue-rotate(90deg)';
+document.getElementById('hue-rotateImg').style.WebkitFilter = 'hue-rotate(90deg)';
 });
 
 invertSelect.addEventListener('click', function(){
-document.getElementById('invert').style.filter = 'invert(100%)';
+document.getElementById('invertImg').style.filter = 'invert(100%)';
 // Code for Safari 6.0 - 9.0
-document.getElementById('invert').style.WebkitFilter = 'invert(100%)';
+document.getElementById('invertImg').style.WebkitFilter = 'invert(100%)';
 });
 
 opacitySelect.addEventListener('click', function(){
-document.getElementById('opacity').style.filter = 'opacity(30%)';
+document.getElementById('opacityImg').style.filter = 'opacity(30%)';
 // Code for Safari 6.0 - 9.0
-document.getElementById('opacity').style.WebkitFilter = 'opacity(30%)';
+document.getElementById('opacityImg').style.WebkitFilter = 'opacity(30%)';
 });
 
 saturateSelect.addEventListener('click', function(){
-  document.getElementById('saturate').style.filter = 'saturate(8)';
-  document.getElementById('saturate').style.WebkitFilter = 'saturate(8)';
+  document.getElementById('saturateImg').style.filter = 'saturate(8)';
+  document.getElementById('saturateImg').style.WebkitFilter = 'saturate(8)';
 });
 
 sepiaSelect.addEventListener('click', function(){
-  document.getElementById('sepia').style.filter = 'sepia(100%)';
-  document.getElementById('sepia').style.WebkitFilter = 'sepia(100%)';
+  document.getElementById('sepiaImg').style.filter = 'sepia(100%)';
+  document.getElementById('sepiaImg').style.WebkitFilter = 'sepia(100%)';
 });
 
 photofilterSelect.addEventListener('click', function(){
-  document.getElementById('photofilter').style.filter = 'hue-rotate(270deg)';
-  document.getElementById('photofilter').style.WebkitFilter = 'hue-rotate(270deg)';
+  document.getElementById('photofilterImg').style.filter = 'hue-rotate(270deg)';
+  document.getElementById('photofilterImg').style.WebkitFilter = 'hue-rotate(270deg)';
 });
 
 noneSelect.addEventListener('click', function(){
-  document.getElementById('none').style.filter = 'none';
-  document.getElementById('none').style.WebkitFilter = 'none';
+  document.getElementById('noneImg').style.filter = 'none';
+  document.getElementById('noneImg').style.WebkitFilter = 'none';
 });
 
--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,66 +1,66 @@
 `use strict`
 
-const blur = document.getElementById('blurSelect');
-const sepia = document.getElementById('sepiaSelect');
-const opacity = document.getElementById('opacitySelect');
-const contrast = document.getElementById('contrastSelect');
-const hueRotate = document.getElementById('hue-rotateSelect');
-const dropShadow = document.getElementById('drop-shadowSelect');
-const none = document.getElementById('noneSelect');
-const invert = document.getElementById('invertSelect');
-const saturate = document.getElementById('saturateSelect');
-const grayscale = document.getElementById('grayscaleSelect');
-const brightness = document.getElementById('brightnessSelect');
-const photofilter = document.getElementById('photofilterSelect');
-const filter = document.getElementById('filtro');
+const blurSelect = document.getElementById('blur');
+const sepiaSelect = document.getElementById('sepia');
+const opacitySelect = document.getElementById('opacity');
+const contrastSelect = document.getElementById('contrast');
+const hueRotateSelect = document.getElementById('hue-rotate');
+const dropShadowSelect = document.getElementById('drop-shadow');
+const noneSelect = document.getElementById('none');
+const invertSelect = document.getElementById('invert');
+const saturateSelect = document.getElementById('saturate');
+const grayscaleSelect = document.getElementById('grayscale');
+const brightnessSelect = document.getElementById('brightness');
+const photofilterSelect = document.getElementById('photofilter');
+const filterSelect = document.getElementById('filtro');
 
 blurSelect.addEventListener('click', function(event){
-  filter.style.filter = "blur(5px)";
-  filter.style.WebkitFilter = "blur(5px)"; 
+  filterSelect.style.filter = "blur(5px)";
+  filterSelect.style.WebkitFilter = "blur(5px)"; 
 });
 sepiaSelect.addEventListener('click', function(fil){
-  filter.style.filter = "sepia(100%)";
-  filter.style.WebkitFilter = "sepia(100%)";
+  filterSelect.style.filter = "sepia(100%)";
+  filterSelect.style.WebkitFilter = "sepia(100%)";
 });
 opacitySelect.addEventListener('click', function(){
-  filter.style.filter = "opacity(50%)";
-  filter.style.WebkitFilter = "opacity(50%)";
+  filterSelect.style.filter = "opacity(50%)";
+  filterSelect.style.WebkitFilter = "opacity(50%)";
 })
 contrastSelect.addEventListener('click', function(){
-  filter.style.filter = "contrast(200%)";
-  filter.style.WebkitFilter = "contrast(200%)";
+  filterSelect.style.filter = "contrast(200%)";
+  filterSelect.style.WebkitFilter = "contrast(200%)";
 })
 hueRotateSelect.addEventListener('click', function(){
-  filter.style.filter = "hue-rotate(90deg)";
-  filter.style.WebkitFilter = "hue-rotate(90deg)";
+  filterSelect.style.filter = "hue-rotate(90deg)";
+  filterSelect.style.WebkitFilter = "hue-rotate(90deg)";
 })
 dropShadowSelect.addEventListener('click', function(){
-  filter.style.filter = "drop-shadow(8px 8px 10px gray)";
-  filter.style.WebkitFilter = "drop-shadow(8px 8px 10px teal)";
+  filterSelect.style.filter = "drop-shadow(8px 8px 10px gray)";
+  filterSelect.style.WebkitFilter = "drop-shadow(8px 8px 10px teal)";
 })
 noneSelect.addEventListener('click', function(){
-  filter.style.filter = "none";
-  filter.style.WebkitFilter = "none";
+  filterSelect.style.filter = "none";
+  filterSelect.style.WebkitFilter = "none";
 })
 invertSelect.addEventListener('click', function(){
-  filter.style.filter = "invert(100%)";
-  filter.style.WebkitFilter = "invert(100%)";
+  filterSelect.style.filter = "invert(100%)";
+  filterSelect.style.WebkitFilter = "invert(100%)";
 })
 saturateSelect.addEventListener('click', function(){
-  filter.style.filter = "saturate(8)";
-  filter.style.WebkitFilter = "saturate(8)";
+  filterSelect.style.filter = "saturate(8)";
+  filterSelect.style.WebkitFilter = "saturate(8)";
 })
 grayscaleSelect.addEventListener('click', function(){
-  filter.style.filter = "grayscale(100%)";
-  filter.style.WebkitFilter = "grayscale(100%)";
+  filterSelect.style.filter = "grayscale(100%)";
+  filterSelect.style.WebkitFilter = "grayscale(100%)";
 })
 brightnessSelect.addEventListener('click', function(){
-  filter.style.filter = "brightness(200%)";
-  filter.style.WebkitFilter = "brightness(200%)";
+  filterSelect.style.filter = "brightness(200%)";
+  filterSelect.style.WebkitFilter = "brightness(200%)";
 })
 photofilterSelect.addEventListener('click', function(){
-  filter.style.filter = "hue-rotate(270deg)";
-  filter.style.WebkitFilter = "hue-rotate(270deg)";
+  filterSelect.style.filter = "hue-rotate(270deg)";
+  filterSelect.style.WebkitFilter = "hue-rotate(270deg)";
 })
 /*blur.addEventListener('click', function(){
 var thumb = document.getElementById('blah').style.filter = "blur(5px)";
