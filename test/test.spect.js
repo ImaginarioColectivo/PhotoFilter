9e8316dcb8321bea160463b578e9ec683c848464
ImaginarioColectivo <marjourzua@gmail.com>
2018-02-01 14:10:11 -0300

    avance test.spect.js

---
+++ test/test.spect.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+let dom = new JSDOM('<!doctype html><html><body><input id="testito"></input></body></html>');
+const { window } = dom;
+global.document = dom;
+global.window = window;
+global.navigator = {
+    userAgent: 'node.js',
+};
+
+const blurSelect = require('')
