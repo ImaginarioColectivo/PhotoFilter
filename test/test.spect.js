503e8319ec01f59e290db812db275445f687dfaf
MrsPepa <jose.leon1996@gmail.com>
2018-02-01 18:19:05 -0300

    archivos con error test

--- test/test.spect.js
+++ test/test.spect.js
@@ -4,7 +4,7 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 let dom = new JSDOM('<!doctype html><html><body><img src="paisajes.jpg" alt="paisajes" id="filtro"></body></html>');
 const { window } = dom;
-global.document = dom;
+global.document = window.document;
 global.window = window;
 global.navigator = {
     userAgent: 'node.js',
@@ -12,9 +12,9 @@ global.navigator = {
 
 const blurSelect = require('../assets/js/app.js');
 describe("Valida filtro blur", () => {
-	blurSelect("blur");
+	blurSelect("filtro");
 	it("Debe cambiar filtro", () => {
-		chai.assert.equal("blur", "5px");
+		chai.assert.expect("5px").to.equal("5px");
 	})
 })
 module.exports();
